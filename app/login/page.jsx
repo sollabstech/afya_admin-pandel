0fc24675669346aeb954310621601f89b7b449da
Sollabstech <sollabstech@localhost.localdomain>
2026-07-14 17:26:30 +0530

    first commisdfsdt

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation';
 
 const ADMIN_USERNAME = process.env.NEXT_PUBLIC_ADMIN_USERNAME;
 const ADMIN_PASSWORD = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
-
+/////////sdsds////////
 function EyeIcon({ open }) {
   return open ? (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
